f6064370afd1ae4241a213cc723b2e286d1acca8
skv <skv-headless@yandex.ru>
2014-11-28 16:36:12 +0300

    send messages

--- src/scripts/components/RoomPage.jsx
+++ src/scripts/components/RoomPage.jsx
@@ -8,6 +8,9 @@ var React = require('react/addons');
 
 var Users = require('./Users');
 var Messages = require('./Messages');
+var Col = require('react-bootstrap').Col;
+var Input = require('react-bootstrap').Input;
+var Button = require('react-bootstrap').Button;
 
 var RoomPage = React.createClass({
 
@@ -19,17 +22,35 @@ var RoomPage = React.createClass({
       messages: [
           {text: "srgseg"},{text: "srgseg"}
       ],
-      name: ""};
+      name: "",
+      message: ''
+    };
+
   },
 
   render: function () {
     return (
-      <div className='main'>
-        <h1>Hello3!!!</h1>
-        <Users users={this.state.users}/>
-        <Messages messages={this.state.messages}/>
+      <div>
+        <Col xs={6} md={6}>
+          <Users users={this.state.users}/>
+        </Col>
+
+        <Col xs={6} md={6}>
+          <Messages messages={this.state.messages}/>
+        </Col>
+
+        <Input type='text' value={this.state.message} onChange={this.setText}/>
+        <Button bsStyle="info" onClick={this.addMessage}>Send</Button>
       </div>
     );
+  },
+  setText: function(event) {
+    this.setState({message: event.target.value});
+  },
+  addMessage: function() {
+    var messages = this.state.messages;
+    messages.push({text: this.state.message});
+    this.setState({messages: messages});
   }
 });
 
